e91cba39ea20f86a0fee4c7a752e541474c04c6b
Louis Pazos <louispazos@gmail.com>
2018-03-02 01:10:14 +0000

    everything works

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
     var app = angular.module('rental', [ ]);
     
     app.controller('RentalController', function(){
-      
+        this.products = LAPcars;
+    });
 
     
 // Car image
@@ -30,47 +31,57 @@
          year: 2017,
          description: 'Sport, 2 door, all wheel drive, Power comes from a 610-horsepower, 5.2-liter V10 engine.',
          available: 10,
-        //  images: [
-        //      {
-        //          full: 'images/2017-Audi-R8-V10-Plus-1-min.png',
-        //          thumb: 'images/2017-Audi-R8-V10-Plus-1-min.png',
-        //      }
-        //      ],
+         images: [
+             {
+                 full: 'images/2017-Audi-R8-V10-Plus-1-min.png',
+                //  thumb: 'images/2017-Audi-R8-V10-Plus-2-min.png',
+             }
+             ],
              
          booked: 0,
-         price: 560,
+         price: 860,
          rentaCar: true,
          soldOut: true,
          
       },
 
       {
-         make: 'dodge',
-         model: "R8 V10 Plus",
+         make: 'Lamborghini',
+         model: "Centenario",
          year: 2017,
-         description: "Sport, 2 door, all wheel drive, Power comes from a 610-horsepower, 5.2-liter V10 engine.",
-         available: 10,
+         description: "Sport, 2 door, all wheel drive, Power comes from a 759-horsepower, 6.5-liter V12 engine.",
+         available: 2,
+          images: [
+             {
+                 full: 'images/Lamborghini-Centenario1.png',
+                //  thumb: 'images/2017-Audi-R8-V10-Plus-2-min.png',
+             }
+             ],
          booked: 0,
-         price: 560,
+         price: 2560,
          rentaCar: true,
          soldOut: true,
 
       },
 
       {
-         make: "Audi",
-         model: "R8 V10 Plus",
+         make: "Bugatti",
+         model: "Chiron",
          year: 2017,
-         description: "Sport, 2 door, all wheel drive, Power comes from a 610-horsepower, 5.2-liter V10 engine.",
-         available: 10,
+         description: "Super sport, 2 door, all wheel drive, Power comes from a 1479-horsepower, 8.0-liter V10 engine.",
+         available: 2,
+          images: [
+             {
+                 full: 'images/bugatti-chiron-price-in-india-p.jpg',
+                //  thumb: 'images/2017-Audi-R8-V10-Plus-2-min.png',
+             }
+             ],
          booked: 0,
-         price: 560,
+         price: 4500,
          rentaCar: true,
-         soldOut: true,
+         soldOut: false,
 
-      }];
-        this.products = LAPcars;
-    });
+      }]
   
 })();
 // };
