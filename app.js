cdb48eda5cafdc78dc51cb260d3c1ce20f0eb9b2
Louis Pazos <louispazos@gmail.com>
2018-03-03 23:40:56 +0000

    added a form for reviews section, added some style in css file

--- app.js
+++ app.js
@@ -78,11 +78,26 @@
          year: 2017,
          description: "Sport, 2 door, all wheel drive, Power comes from a 759-horsepower, 6.5-liter V12 engine.",
          available: 2,
-          images: [
+         images: [
              {
-                 full: 'images/Lamborghini-Centenario1.png',
+                 full: 'images/Lamborghini-Centenario1small.png',
                 //  thumb: 'images/2017-Audi-R8-V10-Plus-2-min.png',
              }
+             ],
+         reviews: [
+             {
+                 stars: 4,
+                 body: "Sweet Ride",
+                 author: "coolap69@aol.com",
+                 
+             },
+             
+               {
+                 stars: 3,
+                 body: "Alright",
+                 author: "coolap69@aol.com",
+                 
+             },
              ],
          booked: 0,
          price: 2560,
@@ -102,6 +117,14 @@
                  full: 'images/bugatti-chiron-price-in-india-p.jpg',
                 //  thumb: 'images/2017-Audi-R8-V10-Plus-2-min.png',
              }
+             ],
+         reviews: [
+             {
+                 stars: 2,
+                 body: "OK",
+                 author: "coolap69@aol.com",
+                 
+             },
              ],
          booked: 0,
          price: 4500,
