71ca3c2880e7e47a8e268b03415b229a9dcc1b2d
Louis Pazos <louispazos@gmail.com>
2018-03-05 19:02:02 +0000

    Validation email

--- app.js
+++ app.js
@@ -18,6 +18,15 @@
             return this.tab === checkTab;
         };
     });
+    
+    app.controller('ReviewController', function(){
+       this.review = {};
+       
+       this.addReview = function(product) {
+           product.reviews.push(this.review);
+           this.review = {};
+       };
+    });
 
     
 // Car image
@@ -134,35 +143,3 @@
       }]
   
 })();
-// };
-
-// for (var i = 0; i < cars.type.length; i++) {
-//   var options = document.createElement("OPTION");
-//   options.innerHTML = cars.type[i].model;
-//   document.getElementById("optionTags").appendChild(options);
-
-//   options.setAttribute("value", i);
-//   document.getElementById("available").innerHTML = cars.type[0].available;
-//   document.getElementById("price").innerHTML = cars.type[0].price;
-// }
-
-
-// document.getElementById("optionTags").onchange = function() {
-//   var val = document.getElementById("optionTags").value;
-//   val = parseInt(val);
-//   document.getElementById("available").innerHTML = cars.type[val].available;
-//   document.getElementById("price").innerHTML = cars.type[val].price;
-
-// };
-
-// // function rent() {
-// //    var renterName = document.getElementById("fname").value;
-// //    var typeCar = document.getElementById("optionTags") cars.type[].;
-
-// // }
-
-// document.getElementById("rentCar").onsubmit = function(event) {
-//   // console.log ("submit")
-//   event.preventDefault();
-
-// };
